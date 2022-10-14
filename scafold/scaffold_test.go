ab82e16be7da69e2036089a7e7bf8c23cbb4b6eb
Allan Nava <allannava95@gmail.com>
2022-10-14 17:50:52 +0200

    scaffold_test.go

--- scafold/scaffold_test.go
+++ scafold/scaffold_test.go
@@ -1,1 +1,27 @@
 package scaffold
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestScaffold(t *testing.T) {
+    //
+	tempDir, err := ioutil.TempDir(filepath.Join(Gopath, "src"), "test")
+    //
+	if !filepath.IsAbs(tempDir) {
+		tempDir, err = filepath.Abs(tempDir)
+		assert.NoError(t, err)
+	}
+    //
+	fmt.Printf("tempDir:%s\n", tempDir)
+	assert.NoError(t, New(true).Generate(tempDir))
+
+	defer os.RemoveAll(tempDir) // clean up
+}
+//
