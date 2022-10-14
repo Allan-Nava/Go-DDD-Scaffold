6571a4ba66064d79fe12c254324c720ee28e7981
Allan Nava <allannava95@gmail.com>
2022-10-14 17:33:01 +0200

    update main.go

--- main.go
+++ main.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"github.com/urfave/cli"
+)
+
+func main(){
+	app := cli.NewApp()
+	app.Version = "1.0.0-rc"
+	app.Usage = "Generate Scaffold Domain Driven Design project layout for Go."
+
+}
