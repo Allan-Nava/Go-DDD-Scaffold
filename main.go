f08edfcbba21fe4a71527546e1b19dda7428094b
Allan Nava <allannava95@gmail.com>
2022-10-17 15:22:57 +0200

    fix scaffold

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/Allan-Nava/scaffold/scaffold"
+
 	"github.com/urfave/cli"
 )
 
@@ -8,5 +16,29 @@ func main(){
 	app := cli.NewApp()
 	app.Version = "1.0.0-rc"
 	app.Usage = "Generate Scaffold Domain Driven Design project layout for Go."
+	app.Commands = []cli.Command{
+		{
+			Name:    "init",
+			Aliases: []string{"i"},
+			Usage:   " Generate scaffold project layout",
+			Action: func(c *cli.Context) error {
+				currDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+				if err != nil {
+					return err
+				}
+				err = scaffold.New(false).Generate(currDir)
+				//fmt.Printf("error:%+v\n", err)
+				if err == nil {
+					fmt.Println("Success Created. Please excute `make up` to start service.")
+				}
 
+				return err
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
