19aa74280245363d8756e6b72dc002f1515f42ff
Allan Nava <allannava95@gmail.com>
2023-01-18 16:10:39 +0100

    added scaffold template

--- extensions/vscode/src/commands/new-scaffold.command.ts
+++ extensions/vscode/src/commands/new-scaffold.command.ts
@@ -12,9 +12,36 @@ import {
 import { existsSync, lstatSync, writeFile } from "fs";
 //
 export const newScaffold = async (uri: Uri) => {
-
+  const scaffoldName = await promptForScaffoldName();
+  if (_.isNil(scaffoldName) || scaffoldName.trim() === "") {
+    window.showErrorMessage("The scaffold name must not be empty");
+    return;
+  }
+  //
+  let targetDirectory;
+  if (_.isNil(_.get(uri, "fsPath")) || !lstatSync(uri.fsPath).isDirectory()) {
+    targetDirectory = await promptForTargetDirectory();
+    if (_.isNil(targetDirectory)) {
+      window.showErrorMessage("Please select a valid directory");
+      return;
+    }
+  } else {
+    targetDirectory = uri.fsPath;
+  }
+  //
+  try {
+    await generateScaffoldCode(blocName, targetDirectory, blocType);
+    window.showInformationMessage(
+      `Successfully Generated ${scaffoldName} Bloc`
+    );
+  } catch (error) {
+    window.showErrorMessage(
+      `Error:
+        ${error instanceof Error ? error.message : JSON.stringify(error)}`
+    );
+  }
 }
-
+//
 function createScaffoldTemplate(
     scaffoldName: string,
     targetDirectory: string,
@@ -22,4 +49,44 @@ function createScaffoldTemplate(
   ){
     const snakeCaseScaffoldName = changeCase.snakeCase(scaffoldName);
 
+}
+//
+function promptForScaffoldName(): Thenable<string | undefined> {
+  const blocNamePromptOptions: InputBoxOptions = {
+    prompt: "Scaffold Name Name",
+    placeHolder: "GO DDD",
+  };
+  return window.showInputBox(blocNamePromptOptions);
+}
+//
+
+async function generateScaffoldCode(
+  scaffoldName: string,
+  targetDirectory: string,
+  type: BlocType
+) {
+  const shouldCreateDirectory = workspace
+    .getConfiguration("bloc")
+    .get<boolean>("newBlocTemplate.createDirectory");
+  const scaffoldDirectoryPath = shouldCreateDirectory
+    ? `${targetDirectory}/bloc`
+    : targetDirectory;
+  if (!existsSync(scaffoldDirectoryPath)) {
+    await createDirectory(scaffoldDirectoryPath);
+  }
+  //
+  await Promise.all([
+    createScaffoldTemplate(scaffoldName, scaffoldDirectoryPath, type),
+  ]);
+}
+//
+function createDirectory(targetDirectory: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    mkdirp(targetDirectory, (error) => {
+      if (error) {
+        return reject(error);
+      }
+      resolve();
+    });
+  });
 }
---
+++ extensions/vscode/src/templates/index.ts
@@ -0,0 +1,3 @@
+//
+export * from "./scaffold.template";
+//
