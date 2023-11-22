739124b3a247128984b03d6ba9d2a108b8981ec0
Allan Nava <allannava95@gmail.com>
2023-11-22 12:38:48 +0100

    solve main

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #
 echo "is building()"
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -a -o go-scaffold ./...
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -a -o main .
 mkdir -p /tmp/bin
 pwd
 ls -al
-tar -czvf /tmp/bin/go-scaffold.tar.gz --transform='s|.*/||'  ./scaffold 
+tar -czvf /tmp/bin/go-scaffold.tar.gz --transform='s|.*/||'  ./main 
 echo "builded()"
 #tar --ignore-zeros -xf
 ls -l /tmp/bin
