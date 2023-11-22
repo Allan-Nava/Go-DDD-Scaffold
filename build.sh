1b4f78c2e9a2d6df83e1b258a140d980258689f9
Allan Nava <allannava95@gmail.com>
2023-11-22 12:19:37 +0100

    added release also for binary

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -a -o go-scaffol
 mkdir -p /tmp/bin
 pwd
 ls -al
-tar -czvf /tmp/bin/go-scaffold.tar.gz --transform='s|.*/||'  ./go-scaffold 
+tar -czvf /tmp/bin/go-scaffold.tar.gz --transform='s|.*/||'  ./scaffold 
 echo "builded()"
 #tar --ignore-zeros -xf
 ls -l /tmp/bin
